b1b01c948ed5b6e4098b92597b2e5661132cd7ea
Cosmas Egbosi <cosmascj@gmail.com>
2023-09-07 08:15:58 +0100

    tab commit

--- src/navigation/App/Navigators/Tab.tsx
+++ src/navigation/App/Navigators/Tab.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/no-unstable-nested-components */
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable import/extensions */
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { RouteProp, useNavigation } from '@react-navigation/native';
-import { Platform, TouchableOpacity } from 'react-native';
+import { Platform, TouchableOpacity, View } from 'react-native';
 
 
 
@@ -16,44 +16,65 @@ import Icon3 from '../../../assets/icons/Vector.svg'
 import { Text } from '../../../components';
 import { Account, Health, Home, Shedule } from '../../../modules/main';
 import AppNavigator from '../AppNavigator';
+import { ConnectIconIcon } from '../../../assets/icons';
+import { pallets } from '../../../constant/colors';
 const { Navigator, Screen } = createBottomTabNavigator<TabRoutes>();
 
 export default function TabNavigator() {
     const navigation = useNavigation();
-
+    const TabIcon = ({ name, focused, icon }: { name: string, focused: boolean, icon: ReactNode }) => {
+        return (
+            <View style={{ marginStart: 20, borderRadius: 12, paddingVertical: 10, paddingHorizontal: 6, flexDirection: 'row', alignItems: 'center', backgroundColor: focused ? 'rgba(68, 37, 245, 0.1)' : pallets.transparent }}>
+                {icon}
+                {/* {focused && <Text style={{ color: 'blue' }}>{name}</Text>} */}
+                {focused && <Text style={{ marginLeft: 5, color: 'blue' }}>{name}</Text>}
+            </View>
+        );
+    };
 
     return (
         <Navigator
             screenOptions={({ route }) => ({
                 headerShown: false,
-                tabBarIcon: ({ focused, size, color }) => {
-                    const name = getTabIcon(route, focused);
+                tabBarIcon: ({ focused }) => {
+                    const name = getTabIcon(route, focused)
+                    return <TabIcon focused={focused} icon={name} name={route.name} />
+                    // <TabIcon name={route.name} focused={focused} />
+                },
+                // tabBarIcon: ({ focused, size, color }) => {
+                //     const name = getTabIcon(route, focused);
 
-                    return name
-                },
-                tabBarLabel: ({ color }) => {
+                //     return name
+                // },
+                tabBarLabel: ({ color, focused }) => {
                     return (
-                        <Text
-                            style={{
-                                color: color,
-                                fontSize: 10,
-                                padding: Platform.OS === 'android' ? 10 : undefined,
-                            }}>
-                            {route.name}
-                        </Text>
+                        focused ?
+                            null
+                            : null
                     );
+                    // return (
+                    //     <Text
+                    //         style={{
+                    //             color: color,
+                    //             fontSize: 10,
+                    //             padding: Platform.OS === 'android' ? 10 : undefined,
+                    //         }}>
+                    //         {route.name}
+                    //     </Text>
+                    // );
                 },
-                tabBarStyle: [Platform.OS === 'android' && { height: 60, padding: 10 }],
+                tabBarStyle: [Platform.OS === 'android' && { height: 60, padding: 10, flexDirection: 'row' }],
+                // tabBarStyle: { height: 60, margin: 10, },
 
             })
             }
         >
             <>
                 <Screen name="Home" component={AppNavigator} />
-                <Screen name="Profile" component={Account} />
+                <Screen name="Calender" component={Health} />
                 <Screen name="Notification" component={Account} />
                 <Screen name="Shedule" component={Shedule} />
-                <Screen name="Calender" component={Health} />
+                <Screen name="Profile" component={Account} />
 
 
             </>
@@ -68,15 +89,15 @@ const getTabIcon = (
     switch (route.name) {
         case 'Home':
             return focused ? <Icon3 /> : <Icon3 />;
-        case 'Profile':
-            return focused ? <ProfileIcon /> : <ProfileIcon />;
         case 'Notification':
-            return focused ? <CalenderIcon /> : <CalenderIcon />;
-        case 'Shedule':
-            return focused ? <CalenderIcon /> : <CalenderIcon />;
-
+            return focused ? <ConnectIconIcon /> : <ConnectIconIcon />;
         case 'Calender':
             return focused ? <CalenderIcon /> : <CalenderIcon />;
+        case 'Shedule':
+            return focused ? <ShopIcon /> : <ShopIcon />;
+        case 'Profile':
+            return focused ? <ProfileIcon /> : <ProfileIcon />;
+
         default:
             return null;
     }
